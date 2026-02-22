875f20e5c7a97d343d91129419e7b9c12dd9ecef
Mosberg <RasmusTMosberg@gmail.com>
2026-02-22 17:16:28 +0100

    Add logging initialization to VPRClient and VPRDataGenerator

--- src/client/java/dk/mosberg/client/VPRClient.java
+++ src/client/java/dk/mosberg/client/VPRClient.java
@@ -1,11 +1,19 @@
 package dk.mosberg.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.fabricmc.api.ClientModInitializer;
 
 public class VPRClient implements ClientModInitializer {
+  public static final String MOD_ID = "vpr";
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
   @Override
   public void onInitializeClient() {
     // This entrypoint is suitable for setting up client-specific logic, such as
     // rendering.
+
+    LOGGER.info("VPR Client initialized!");
   }
 }
--- src/client/java/dk/mosberg/client/datagen/VPRDataGenerator.java
+++ src/client/java/dk/mosberg/client/datagen/VPRDataGenerator.java
@@ -1,11 +1,18 @@
 package dk.mosberg.client.datagen;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class VPRDataGenerator implements DataGeneratorEntrypoint {
+  public static final String MOD_ID = "vpr";
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
   @Override
   public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+    LOGGER.info("VPR Data Generator initialized!");
   }
 }
