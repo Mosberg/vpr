c178a9016a9cddfa64ac7c46fa5b163e6efe1b02
Mosberg <RasmusTMosberg@gmail.com>
2026-02-22 17:38:43 +0100

    Refactor block registration: rename VPRWarriroPostBlock to VPRWarriorPostBlock and update references; add GUARD_POST field to VPRGuardPostBlock

--- src/main/java/dk/mosberg/block/VPRGuardPostBlock.java
+++ src/main/java/dk/mosberg/block/VPRGuardPostBlock.java
@@ -3,11 +3,15 @@ package dk.mosberg.block;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.block.Block;
+
 public class VPRGuardPostBlock {
   public static final String MOD_ID = "vpr";
   public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+  public static Block GUARD_POST;
 
   public static void register() {
+
     LOGGER.info("Registering Guard Post Block");
   }
 }
--- src/main/java/dk/mosberg/registry/VPRBlocks.java
+++ src/main/java/dk/mosberg/registry/VPRBlocks.java
@@ -4,15 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dk.mosberg.block.VPRGuardPostBlock;
-import dk.mosberg.block.VPRWarriroPostBlock;
+import dk.mosberg.block.VPRWarriorPostBlock;
 
 public class VPRBlocks {
   public static final String MOD_ID = "vpr";
   public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+  private VPRBlocks() {
+    // Private constructor to prevent instantiation
+  }
+
   public static void register() {
+
     VPRGuardPostBlock.register();
-    VPRWarriroPostBlock.register();
+    VPRWarriorPostBlock.register();
 
     LOGGER.info("Registering blocks for " + MOD_ID);
   }
